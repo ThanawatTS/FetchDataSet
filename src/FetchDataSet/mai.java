109c5ceeb2aafd18648f00a2da09ed52c2fcbc8f
GamTS <Spirit_windwalk@hotmail.com>
2017-06-24 15:46:38 +0700

    Create New file = MAI

---
+++ src/FetchDataSet/mai.java
@@ -0,0 +1,19 @@
+package FetchDataSet;
+
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class mai {
+	
+	private JPanel  main;
+	private static JTextArea textArea;
+	private JScrollPane slideDown;
+	
+	static ArrayList<String> nameWeb = new ArrayList<String>();
+	static ArrayList<String> names;
+	
+	
+}
